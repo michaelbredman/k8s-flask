b15edfa9981ecb15530eb57f597d17a2c471f959
Michael Redman <mredman@gmail.com>
2022-12-28 06:36:04 -0800

    Added text

--- app/app.py
+++ app/app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<H1>This is a Hello World application</H1>"
+    return "<H1>This is a Simple application</H1>"
 
 @app.route("/time")
 def hello_time():
-    return "<H1>Time!<H1>"
+    return "<H1>Time function Route<H1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
